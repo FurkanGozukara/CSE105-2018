d0b9649eb405c29892436eb849848a7a23639e98
Furkan Gözükara <furkangozukara@gmail.com>
2018-10-15 11:47:49 +0300

    lecture 5 uploads

---
+++ Source Codes/Lecture 5/Lecture 5/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("i = " + i);
+            }
+
+            Console.WriteLine("");
+
+            for (int k = 5; k < 25; k = k + 2)
+            {
+                Console.WriteLine("k = " + k);
+            }
+
+            int counter = 10;
+
+            for (; counter <= 13; counter+=3)
+            {
+                Console.WriteLine("counter = " + counter);
+            }
+
+            if(counter==16)
+            { 
+                int c = 12;
+                if(c==12)
+                {
+                    int localvariable = 25;
+                }
+            }
+
+            Console.WriteLine("");
+
+            for (int i = 1; i < 100; i++)
+            {
+                Console.WriteLine("i = " + i);
+                var mod = i % 10;
+                if (i % 10 == 0)
+                    break;
+            }
+
+            Console.WriteLine("");
+
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine("i = " + i);
+                for (int k = 1; k < 20; k++)
+                {
+                    Console.WriteLine("k = " + k);
+                    if (k % 17 == 0)
+                        break;
+                }
+            }
+
+            Console.WriteLine("");
+
+            int isPrimeNumber = 179;
+
+            //Is_Not_Prime
+            bool asal_sayi_degil_mi = false;//hayır asal sayı / no not a prime number
+
+            for (int i = 2; i < isPrimeNumber; i++)
+            {
+                if(isPrimeNumber%i==0)
+                {
+                    asal_sayi_degil_mi = true;//evet asal sayi değil / yes it is a prime number
+                    break;
+                }
+            }
+
+            if (asal_sayi_degil_mi == true)
+                Console.WriteLine($"{isPrimeNumber} is not a prime number");
+            else
+                Console.WriteLine($"{isPrimeNumber} is a prime number");
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Source Codes/Lecture_5_2/Lecture_5_2/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            while(i>0)
+            {
+                Console.WriteLine("i=" + i);
+                i--; // i = i -1;
+            }
+
+            i = 10;
+            while (true)
+            {
+                Console.WriteLine("i=" + i);
+                i--; // i = i -1;
+                if (i < 1)
+                    break;
+            }
+
+            Console.Clear();//clears console screen
+
+            while(true)
+            {
+                Console.WriteLine("Please enter an integer to test it is prime number or not?");
+                string userInput = Console.ReadLine();
+
+                Int64 intCheck = 0;
+                Int64.TryParse(userInput, out intCheck);//tries to parse as integer
+                
+                if(intCheck<2)
+                {
+                    Console.WriteLine("Please enter an positive integer bigger than 1");
+                    Console.WriteLine("");
+                    continue;
+                }
+
+                bool isAPrimeNumber = check_Is_Prime(intCheck);
+
+                if (isAPrimeNumber == false)
+                    Console.WriteLine($"[{intCheck}] is not a prime number");
+                else
+                    Console.WriteLine($"[{intCheck}] is a prime number");
+
+                Console.WriteLine("");
+
+               // break;//break the infinite loop if you wish
+            }
+
+            bool result = check_Is_Prime(31242);
+
+            Console.ReadLine();
+        }
+
+        //private protected public : güvenlik seviyesi accessbility
+        private static bool check_Is_Prime(Int64 numberToCheck)
+        {
+            bool asal_sayi_degil_mi = false;
+
+            for (int i = 2; i < numberToCheck; i++)
+            {
+                if (numberToCheck % i == 0)
+                {
+                    asal_sayi_degil_mi = true;
+                    //evet asal sayi değil / yes it is a prime number
+                    break;
+                }
+            }
+
+            // makes true > false , false > true by using ! infront
+            asal_sayi_degil_mi = !asal_sayi_degil_mi;
+
+            return asal_sayi_degil_mi;
+        }
+    }
+}
